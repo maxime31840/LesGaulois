a3ce79beae077993959e5874202cd9241047e22e
maxime31840 <maxime.lourdelus@gmail.com>
2024-03-01 11:38:21 +0100

    début tp2

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -3,18 +3,34 @@ package personnages;
 public class Village {
 	private String nom;
 	private Chef chef;
-	
+	private Gaulois[] villageois;
+	private int nbVillageois;
+
 	public Village(String nom, Chef chef) {
 		this.nom = nom;
+		this.villageois = 0;
 		this.chef = chef;
+		this.villageois = new Gaulois[100];
 	}
-	
+
 	public String getNom() {
 		return nom;
 	}
-	
+
 	public void setChef(Chef chef) {
 		this.chef = chef;
 	}
 
+	public void ajouterVillagesois(Gaulois villageois) {
+		this.villageoi[this.villageois] = villageois;
+		this.nbVillageois++;
+	}
+
+	public Gaulois[] getVillageois() {
+		return villageois;
+	}
+
+	public int getVillageois() {
+		return nbVillageois;
+	}
 }
