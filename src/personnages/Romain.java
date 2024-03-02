33350fd276e0f6a91a8d26473faf556d6b3e3615
maxime31840 <maxime.lourdelus@gmail.com>
2024-03-02 14:03:12 +0100

    enum

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,11 +3,15 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement[] equipements;
+	private int nbEquipement;
 
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		assert force >= 0 : "La force d'un Romain doit être positive";
 		this.force = force;
+		this.equipements = new Equipement[2];
+		this.nbEquipement = 0;
 	}
 
 	public String getNom() {
@@ -34,8 +38,38 @@ public class Romain {
 
 	}
 
+	private void ajouterEquipement(Equipement equipement) {
+		equipements[nbEquipement] = equipement;
+		nbEquipement++;
+		System.out.println("Le soldat " + nom + " s'équipe avec un " + equipement + ".");
+	}
+
+	public void sEquiper(Equipement equipement) {
+		switch (nbEquipement) {
+		case 0:
+			ajouterEquipement(equipement);
+			break;
+		case 1:
+			if (equipements[0] == equipement) {
+				System.out.println("Le soldat " + nom + " possède déjà un " + equipement + " !");
+			} else {
+				ajouterEquipement(equipement);
+			}
+			break;
+		case 2:
+			System.out.println("Le soldat " + nom + " est déjà bien protégé !");
+			break;
+		default:
+			System.out.println("Erreur: Nombre d'équipements invalide.");
+		}
+	}
+
 	public static void main(String[] args) {
 		Romain minus = new Romain("minus", 6);
 		minus.parler("Salut");
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.BOUCLIER);
+		minus.sEquiper(Equipement.CASQUE);
 	}
 }
